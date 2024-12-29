c213807cdd3b216058ab79d23a99a82ab2e7a99b
Maxwell DeKock <61523858+max-dekock@users.noreply.github.com>
2024-12-29 10:40:32 -0800

    day 17 part 2

--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -1,8 +1,18 @@
+use nom::{
+    IResult,
+    bytes::complete::tag,
+    character::complete::{u8, u64, line_ending},
+    multi::separated_list1,
+    sequence::{delimited, preceded}
+};
+use itertools::Itertools;
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Registers {
     a: u64,
     b: u64,
-    c: u64
+    c: u64,
+    ip: usize
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -26,13 +36,13 @@ impl Operand {
             4 => Operand::RegA,
             5 => Operand::RegB,
             6 => Operand::RegC,
-            7 => panic!("invalid combo operand")
+            _ => panic!("invalid combo operand")
         }
     }
 
     fn get_value(&self, registers: &Registers) -> u64 {
         match self {
-            Operand::Literal(byte) => byte as u64,
+            Operand::Literal(byte) => *byte as u64,
             Operand::RegA => registers.a,
             Operand::RegB => registers.b,
             Operand::RegC => registers.c
@@ -67,38 +77,146 @@ impl Instruction {
         }
     }
 
-    fn execute(&self, registers: &Registers) -> (Option<u64>, Registers) {
+    fn execute(&self, registers: &Registers) -> (Option<u8>, Registers) {
         match self {
             Instruction::Adv(operand) => {
                 let numerator = registers.a;
                 let denominator = 1 << operand.get_value(registers);
-                (None, Registers { ..registers, a: numerator / denominator })
+                let new_registers = Registers {
+                    a: numerator / denominator,
+                    ip: registers.ip + 2,
+                    ..*registers
+                };
+                (None, new_registers)
             },
             Instruction::Bdv(operand) => {
-                let numerator = registers.b;
+                let numerator = registers.a;
                 let denominator = 1 << operand.get_value(registers);
-                (None, Registers { ..registers, b: numerator / denominator })
+                let new_registers = Registers {
+                    b: numerator / denominator,
+                    ip: registers.ip + 2,
+                    ..*registers
+                };
+                (None, new_registers)
             },
             Instruction::Cdv(operand) => {
-                let numerator = registers.c;
+                let numerator = registers.a;
                 let denominator = 1 << operand.get_value(registers);
-                (None, Registers { ..registers, c: numerator / denominator })
+                let new_registers = Registers {
+                    c: numerator / denominator,
+                    ip: registers.ip + 2,
+                    ..*registers
+                };
+                (None, new_registers)
             },
             Instruction::Bxl(operand) => {
-                (None, Registers { ..registers, b: registers.b ^ operand.get_value() })
+                let new_registers = Registers {
+                    b: registers.b ^ operand.get_value(registers),
+                    ip: registers.ip + 2,
+                    ..*registers
+                };
+                (None, new_registers)
             },
             Instruction::Bst(operand) => {
-                (None, Registers { ..
+                let new_registers = Registers {
+                    b: operand.get_value(registers) % 8,
+                    ip: registers.ip + 2,
+                    ..*registers
+                };
+                (None, new_registers)
+            },
+            Instruction::Jnz(operand) => {
+                let ip = if registers.a == 0 { registers.ip + 2 } else { operand.get_value(registers) as usize };
+                let new_registers = Registers {
+                    ip,
+                    ..*registers
+                };
+                (None, new_registers)
+            },
+            Instruction::Bxc => {
+                let new_registers = Registers {
+                    b: registers.b ^ registers.c,
+                    ip: registers.ip + 2,
+                    ..*registers
+                };
+                (None, new_registers)
+            },
+            Instruction::Out(operand) => {
+                let new_registers = Registers {
+                    ip: registers.ip + 2,
+                    ..*registers
+                };
+                let output = (operand.get_value(registers) % 8) as u8;
+                (Some(output), new_registers)
+            }
+        }
+    }
+}
 
+fn execute_code(initial_registers: &Registers, code: &[u8]) -> Vec<u8> {
+    let mut registers = *initial_registers;
+    let mut out_buf = vec![];
+    while registers.ip <= code.len() - 2 {
+        let opcode = code[registers.ip];
+        let operand = code[registers.ip + 1];
+        let instruction = Instruction::parse(opcode, operand);
+        //println!("{:?}", instruction);
+        let (output, new_registers) = instruction.execute(&registers);
+        if let Some(output) = output {
+            out_buf.push(output);
+        }
+        registers = new_registers;
+        //println!("{:#?}", registers);
+    }
+    out_buf
+}
+
+fn parse_input(input: &str) -> IResult<&str, (Registers, Vec<u8>)> {
+    let (input, a) = delimited(tag("Register A: "), u64, line_ending)(input)?;
+    let (input, b) = delimited(tag("Register B: "), u64, line_ending)(input)?;
+    let (input, c) = delimited(tag("Register C: "), u64, line_ending)(input)?;
+    let (input, _) = line_ending(input)?;
+    let (input, code) = preceded(tag("Program: "), separated_list1(tag(","), u8))(input)?;
+    let registers = Registers { a, b, c, ip: 0 };
+    Ok((input, (registers, code)))
 }
 
 fn part_1(input: &str) -> String {
+    let (_, (registers, code)) = parse_input(input).unwrap();
+    let output = execute_code(&registers, &code);
+    output.into_iter().join(",")
+}
 
+// by reverse engineering the input, i've deduced that every 3 bits in A results in one output
+// we can figure out the correct value by backtracking
+fn find_quine(code: &[u8], thus_far: u64, idx: usize) -> Option<u64> {
+    for x in 0..=7 {
+        let a = thus_far << 3 | x;
+        let registers = Registers { a, b: 0, c: 0, ip: 0 };
+        let output = execute_code(&registers, code);
+        if output[0] == code[idx] {
+            if idx == 0 {
+                return Some(a);
+            }
+            if let Some(solution) = find_quine(code, a, idx - 1) {
+                return Some(solution);
+            }
+        }
+    }
+    None
+}
+
+fn part_2(input: &str) -> u64 {
+    let (_, (_, code)) = parse_input(input).unwrap();
+    if let Some(solution) = find_quine(&code, 0, code.len() - 1) {
+        return solution;
+    }
+    panic!("couldn't find a solution!");
 }
 
 fn main() {
     let input = std::io::read_to_string(std::io::stdin()).unwrap();
 
     println!("Part 1: {}", part_1(&input));
-    //println!("Part 2: {}", part_2(&input));
+    println!("Part 2: {}", part_2(&input));
 }
